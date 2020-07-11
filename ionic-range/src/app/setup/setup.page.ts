893f507a262f51f2bedb4ad455f7d3deff2f4399
sskriblo <a.skriblo@gmail.com>
2020-07-11 22:15:58 +0300

    in progress - try to get input value not final yet

--- ionic-range/src/app/setup/setup.page.ts
+++ ionic-range/src/app/setup/setup.page.ts
@@ -7,13 +7,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./setup.page.scss'],
 })
 export class SetupPage implements OnInit {
-
+  comfortInpVal: string = '';
+  economInpVal: string = '';
   constructor( public router: Router) { }
 
   ngOnInit() {
   }
   toHomePage() {
     this.router.navigate(['home']);  
+  }
+
+  comfortInput() {
+    console.log(this.comfortInpVal)
   }
 
 }
