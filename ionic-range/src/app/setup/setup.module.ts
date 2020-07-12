fd5e00f31fc70b2e68421954841053413f96514b
sskriblo <a.skriblo@gmail.com>
2020-07-12 21:29:24 +0300

    add storage service, not finished yet

--- ionic-range/src/app/setup/setup.module.ts
+++ ionic-range/src/app/setup/setup.module.ts
@@ -1,20 +1,41 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { SetupPageRoutingModule } from './setup-routing.module';
-
 import { SetupPage } from './setup.page';
+import { Storage } from '@ionic/storage';
+import { IonicStorageModule } from '@ionic/storage';
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    SetupPageRoutingModule
+    SetupPageRoutingModule,
+    IonicStorageModule.forRoot({
+      name: '__mydb',
+      driverOrder: ['indexeddb', 'sqlite', 'websql']
+    })
   ],
   declarations: [SetupPage]
 })
-export class SetupPageModule {}
+export class SetupPageModule {
+  constructor(private storage: Storage) { }
+
+  updateComfortT() {
+
+  }
+
+  getComfortT() {
+  // set a key/value
+  this.storage.set('name', 'Max');
+
+  // Or to get a key/value pair
+  this.storage.get('age').then((val) => {
+    console.log('Your age is', val);
+  });  
+  }
+  
+}
