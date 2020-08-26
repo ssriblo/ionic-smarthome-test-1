08b606824d32c6eb994c560aaf18389b0fa6f1b6
sskriblo <a.skriblo@gmail.com>
2020-08-26 11:35:15 +0300

    add new Alert to confirm De-registration or cancel it

--- ionic-range/src/app/setup/setup.page.ts
+++ ionic-range/src/app/setup/setup.page.ts
@@ -76,9 +76,37 @@ export class SetupPage implements OnInit {
     this.apiService.removeJwt().then(val => {
 //    this.storage.remove('jwtString').then(val => { 
       console.log('[deregistered]: remove jwtString from Local Store', val)
-      this.apiService.isJWT = false;
-      this.router.navigate(['introduction']) 
+      this.presentAlert();
+      // this.apiService.isJWT = false;
+      // this.router.navigate(['introduction']) 
     });
   }
 
+  private async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      animated: true,
+      backdropDismiss: true,
+      header: 'Внимание',
+      message: 'Вы уверены, что надо удалить текущую регистрацию? Это действие нельзя будет отменить!" ',
+      buttons: [
+        {
+          text: 'Отмена',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Новая регистрация',
+          handler: () => {
+            console.log('New Registration clicked');
+            this.apiService.isJWT = false;
+            this.router.navigate(['introduction'])       
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
 }
