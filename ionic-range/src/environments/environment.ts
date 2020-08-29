fc9d5795b5521bbdb5bea0a37f483e6c781b94d2
sskriblo <a.skriblo@gmail.com>
2020-08-29 18:05:20 +0300

    add apk local/cloud files only

--- ionic-range/src/environments/environment.ts
+++ ionic-range/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   mobileBuild: false, // setup FALSE for Chrome and TRUE for Android
   version: "(v5) ",
-  serverLoc: "local", // options: ["local", "cloud"]
+  serverLoc: "cloud", // options: ["local", "cloud"]
   SERVER_URL_LOCAL: 'http://localhost:8080/OVK/OVK_mob1/1.0.7/',
   SERVER_URL_GOOGLE: 'https://web-serv13802.nw.r.appspot.com/',
   JWT_DEFAULT: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGFydElEIjoiMTExIiwibmFtZSI6ItCh0LXRgNCz0LXQuSDQoSIsInRva2VuTnVtYmVyIjoxLCJwcm9qZWN0IjoidGVzdFByb2plY3QtMSIsImlhdCI6MTU5NzczMjY3NiwiZXhwIjozODA2ODU2ODc5fQ.b9rTPTEiBTo-eexqA14TOPP66u0-nWOkjPEFc3047Gk'
