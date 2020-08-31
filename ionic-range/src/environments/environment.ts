7199f5d319bae23de6de60e83a8441118c1dfe67
sskriblo <a.skriblo@gmail.com>
2020-08-31 18:58:13 +0300

    new Firestore project smart-home-288104

--- ionic-range/src/environments/environment.ts
+++ ionic-range/src/environments/environment.ts
@@ -5,10 +5,11 @@
 export const environment = {
   production: false,
   mobileBuild: false, // setup FALSE for Chrome and TRUE for Android
-  version: "(v6.cloud) ",
+  version: "(v7.cloud) ",
   serverLoc: "cloud", // options: ["local", "cloud"]
   SERVER_URL_LOCAL: 'http://localhost:8080/OVK/OVK_mob1/1.0.7/',
-  SERVER_URL_GOOGLE: 'https://web-serv13802.nw.r.appspot.com/',
+// this project banned and disabled SERVER_URL_GOOGLE: 'https://web-serv13802.nw.r.appspot.com:8080/OVK/OVK_mob1/1.0.7/',
+  SERVER_URL_GOOGLE: 'https://smart-home-288104.ew.r.appspot.com:8080/OVK/OVK_mob1/1.0.7/',
   JWT_DEFAULT: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGFydElEIjoiMTExIiwibmFtZSI6ItCh0LXRgNCz0LXQuSDQoSIsInRva2VuTnVtYmVyIjoxLCJwcm9qZWN0IjoidGVzdFByb2plY3QtMSIsImlhdCI6MTU5NzczMjY3NiwiZXhwIjozODA2ODU2ODc5fQ.b9rTPTEiBTo-eexqA14TOPP66u0-nWOkjPEFc3047Gk'
 
 };
