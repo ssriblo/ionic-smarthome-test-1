5cd88f0d84d7b6b1282066f2a343d9112d2bfbe2
sskriblo <a.skriblo@gmail.com>
2020-09-01 12:25:24 +0300

    Make autostart for API Server. Mob App does not work with server, do'nt know why... Chrome App works with server well. May be CORS is cause? This is V7.cloud version

---
+++ python-flask-server-generated/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+echo
+echo '>>>>>>>>>>>>>>>>>> API Swagger Server Start now >>>>>>>>>>>>>>>'
+echo
+cd /home/sskriblo/Work/ionic-smarthome-test-1/python-flask-server-generated/
+source env/bin/activate
+python3 -m swagger_server
