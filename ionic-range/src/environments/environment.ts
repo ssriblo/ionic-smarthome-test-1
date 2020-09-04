040f3c802e9069be44142f799674ce6ee0275e16
sskriblo <a.skriblo@gmail.com>
2020-09-04 14:01:23 +0300

    test pyopenssl ssl connect

--- ionic-range/src/environments/environment.ts
+++ ionic-range/src/environments/environment.ts
@@ -12,7 +12,8 @@ export const environment = {
 // this project banned and disabled SERVER_URL_GOOGLE: 'https://web-serv13802.nw.r.appspot.com:8080/OVK/OVK_mob1/1.0.7/',
 // SERVER_URL_GOOGLE: 'https://smart-home-288104.ew.r.appspot.com:8080/OVK/OVK_mob1/1.0.7/',
 //  SERVER_URL_GOOGLE: 'http://84.201.154.208:8080/OVK/OVK_mob1/1.0.7/',
-  SERVER_URL_GOOGLE: 'http://84.201.154.208:8080/', 
+//  SERVER_URL_GOOGLE: 'http://84.201.154.208:8080/', 
+  SERVER_URL_GOOGLE: 'https://84.201.154.208:8080/', 
   
   JWT_DEFAULT: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGFydElEIjoiMTExIiwibmFtZSI6ItCh0LXRgNCz0LXQuSDQoSIsInRva2VuTnVtYmVyIjoxLCJwcm9qZWN0IjoidGVzdFByb2plY3QtMSIsImlhdCI6MTU5NzczMjY3NiwiZXhwIjozODA2ODU2ODc5fQ.b9rTPTEiBTo-eexqA14TOPP66u0-nWOkjPEFc3047Gk'
 
--- web_serv/main.py
+++ web_serv/main.py
@@ -170,6 +170,7 @@ if __name__ == '__main__':
     # the "static" directory. See:
     # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
     # App Engine itself will serve those files as configured in app.yaml.
-    app.run(host='0.0.0.0', port=8080, debug=False)
+#    app.run(host='0.0.0.0', port=8080, debug=False)
 #    app.run(port=8080, debug=False)
+    app.run( host='0.0.0.0', port=8080, ssl_context = 'adhoc')  # Generate Adhoc Certs
 
