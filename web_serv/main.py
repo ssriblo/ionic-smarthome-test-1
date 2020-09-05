23fc67d4a0fb5e371e76c1af56477cfa08125ca1
sskriblo <a.skriblo@gmail.com>
2020-09-05 16:39:25 +0300

    fix again...

--- web_serv/main.py
+++ web_serv/main.py
@@ -177,7 +177,6 @@ if __name__ == '__main__':
     # the "static" directory. See:
     # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
     # App Engine itself will serve those files as configured in app.yaml.
-#    app.run(host='0.0.0.0', port=8080, debug=False, ssl_context=context)
-    app.run( host='127.0.0.1', port=8080, debug=False, ssl_context=context)
+    app.run(host='0.0.0.0', port=8080, debug=False, ssl_context=context)
 #    app.run(port=8080, debug=False)
 
