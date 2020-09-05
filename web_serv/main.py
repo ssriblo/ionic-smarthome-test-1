638dc50210a2fe6d2443b620b7c6a0b67947233a
sskriblo <a.skriblo@gmail.com>
2020-09-05 16:33:14 +0300

    new host=127.0.0.1 for SSL

--- web_serv/main.py
+++ web_serv/main.py
@@ -178,6 +178,6 @@ if __name__ == '__main__':
     # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
     # App Engine itself will serve those files as configured in app.yaml.
 #    app.run(host='0.0.0.0', port=8080, debug=False, ssl_context=context)
-    app.run( port=8080, debug=False, ssl_context=context)
+    app.run( host='127.0.0.1' port=8080, debug=False, ssl_context=context)
 #    app.run(port=8080, debug=False)
 
