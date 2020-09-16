bce9e20e3d074007cb82d53166a443993e8ade09
sskriblo <a.skriblo@gmail.com>
2020-09-16 13:09:18 +0300

    remove swagger server only

--- python-flask-server-generated/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-echo
-echo '>>>>>>>>>>>>>>>>>> API Swagger Server Start now >>>>>>>>>>>>>>>'
-echo
-cd /home/sskriblo/Work/ionic-smarthome-test-1/python-flask-server-generated/
-source env/bin/activate
-python3 -m swagger_server
